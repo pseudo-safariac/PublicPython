4526badad70cd1ead7fedc1c47c88f6041c90b8e
Kevin_Orthoptera <84272235+pseudo-safariac@users.noreply.github.com>
2022-03-22 16:42:42 +0300

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -5,3 +5,20 @@ It is my sole mission and purpose to add as many ways as I can creatively think 
 """
 
 print('hello World')  #This is the only one that is unique since it is most famous
+
+	
+#print("\033[1;32;40m Bright Green  \n")
+class FancyWord:
+    def __init__(self, word):
+        self.word = word
+    
+    def underlining_word(self):
+        return f'\033[4m {self.word}'
+        
+    
+        
+
+text = 'Hello World'       
+
+first = FancyWord(text)
+print(first.underlining_word())
