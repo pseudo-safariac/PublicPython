f72db8b51970894010093003d8c0f31f9725752b
Kevin_Orthoptera <84272235+pseudo-safariac@users.noreply.github.com>
2022-03-22 09:29:38 +0300

    Update and rename Hello_World to Hello_World.py

---
+++ Hello_World.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+this are the many ways of creatively saying Hello World in Python. 
+It is my sole mission and purpose to add as many ways as I can creatively think of.
+"""
+
+print('hello World')  #This is the only one that is unique since it is most famous
