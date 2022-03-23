cca21f3ec4d54da4dc1afdd95c39ffc0e7fa508a
Kevin_Orthoptera <84272235+pseudo-safariac@users.noreply.github.com>
2022-03-23 18:39:35 +0300

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
+
 """
-this are the many ways of creatively saying Hello World in Python. 
+this are the many ways I could think of saying Hello World in Python. 
 It is my sole mission and purpose to add as many ways as I can creatively think of.
 """
 
-print('hello World')  #This is the only one that is unique since it is most famous
+__author__ = "Kevin Ngigi"
+__copyright__ = "Copyright (C) 2022 Kevin Ngigi"
+__license__ = "Apache License 2.0"
+__maintainer__ = "Kevin Ngigi"
+__email__ = "kevinngigi110@gmail.com"
 
-	
-#print("\033[1;32;40m Bright Green  \n")
-class FancyWord:
+class Game:
     def __init__(self, word):
         self.word = word
-    
+        
+    def vanilla_word(self):
+        return self.word
+        
     def underlining_word(self):
         return f'\033[4m {self.word}'
         
-    
-        
-
-text = 'Hello World'       
-
-first = FancyWord(text)
-print(first.underlining_word())
+if __name__ == '__main__':
+    user_input = 'Hello World!'
+    app = Game(user_input)
