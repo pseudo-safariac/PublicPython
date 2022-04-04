c917f996b971cbc86739b5515932271c2aac9a9a
Kevin_Orthoptera <kevinngigi110@gmail.com>
2022-04-04 16:49:36 +0300

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import string
+
 
 """
 this are the many ways I could think of saying Hello World in Python. 
@@ -11,6 +13,7 @@ __license__ = "Apache License 2.0"
 __maintainer__ = "Kevin Ngigi"
 __email__ = "kevinngigi110@gmail.com"
 
+
 class Game:
     def __init__(self, word):
         self.word = word
@@ -20,6 +23,18 @@ class Game:
         
     def underlining_word(self):
         return f'\033[4m {self.word}'
+	
+	def string_module(word):
+    #using the random function
+    holding_variable = []
+    small = string.ascii_letters
+    punctuation = string.punctuation
+    for x in word:
+        if x in small or punctuation:
+            holding_variable.append(x)
+        else:
+            return f'{x} is not'
+    return ''.join(holding_variable)
         
 if __name__ == '__main__':
     user_input = 'Hello World!'
